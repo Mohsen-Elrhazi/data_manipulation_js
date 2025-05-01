22a46419d57da5f1677d1ca93a1f6a469385dd63
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-05-01 22:51:58 +0100

    terminer ex25/Medium

--- Medium/ex25.js
+++ Medium/ex25.js
@@ -5,3 +5,33 @@ const { employees } = require("../data.js");
 // Input: employees
 // Output: { "Développement": 3.2, "Design": 2.5, "Marketing": 1.8, "Management": 2.7 }
 // Expected Result: Returns an object with the average number of projects per employee for each department.
+
+function uniqueDepartment(employees){
+    let array=[];
+    for(let i=0;i<employees.length;i++){
+        if(!array.includes(employees[i].department)){
+            array.push(employees[i].department);
+        }
+    }
+    return array;
+}
+
+function calculateAverageProjectsByDepartment(employees){
+    arrayDeparts=uniqueDepartment(employees);
+    let obj={};
+    let count;
+    let sum=0;
+    for(let i=0; i<arrayDeparts.length;i++){
+        count=0;
+        sum=0;
+        for(let j=0; j<employees.length; j++){
+            if(arrayDeparts[i]===employees[j].department){
+                sum+= employees[j].projects.length;
+                count++;
+            }
+        }
+        obj[arrayDeparts[i]]=sum/count;
+    }
+    return obj;
+}
+console.log( calculateAverageProjectsByDepartment(employees));
