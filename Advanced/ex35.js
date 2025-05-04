f2a8cead8b47a0e3d7d67cf9a41b87a1cfac44b7
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-05-04 22:26:45 +0100

    terminer ex35/Advanced

--- Advanced/ex35.js
+++ Advanced/ex35.js
@@ -5,18 +5,20 @@ const { employees } = require("../data.js");
 //     - **Output**: `{ "D": [ { id: 1, ... }, { id: 10, ... } ], "M": [ { id: 2, ... }, { id: 9, ... } ], ... }`
 //     - **Expected Result**: Returns an object grouping employees by the first letter of their last name.
 
-function groupEmployeesByLastNameInitial(employees){
-    let obj={};
-    let array=[];
-    for(let i=0; i<employees.length;i++){
-        for(let j=i; j<employees.length; j++){
-            if(employees[i].lastName[0]===employees[j].lastName[0] ){
-                array.push(employees[j]);
-            }
+function groupEmployeesByLastNameInitial(employees) {
+    let obj = {};  
+
+    for (let i = 0; i < employees.length; i++) {
+        let initiale = employees[i].lastName[0]; 
+
+        if (!obj[initiale]) {
+            obj[initiale] = [];  
         }
-        obj[employees[i].lastName[0]]=array;
+
+        obj[initiale].push(employees[i]);
     }
-    return obj;
+
+    return obj;  
 }
 
 console.log( groupEmployeesByLastNameInitial(employees))
